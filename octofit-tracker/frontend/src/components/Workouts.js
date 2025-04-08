2aa30402a73801baaeb59cfd580eb157baa8bc0a
ajaybalaji24 <ajay.vivagoals@gmail.com>
2025-04-08 07:57:10 +0000

    Commit all changes

---
+++ octofit-tracker/frontend/src/components/Workouts.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Workouts() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://friendly-space-spork-r4r5xqqjpq652v9j-8000.app.github.dev/api/workouts')
+      .then(response => response.json())
+      .then(data => setWorkouts(data));
+  }, []);
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h1 className="card-title">Workouts</h1>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {workouts.map(workout => (
+              <tr key={workout.id}>
+                <td>{workout.id}</td>
+                <td>{workout.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Workouts;
